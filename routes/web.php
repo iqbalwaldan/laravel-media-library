848cf72ee7cc6d6e7f4cd5a3e4ac80473c65cf9b
iqbalwaldan <iqbalwaldan.social@gmail.com>
2023-09-17 07:06:05 +0700

    Setup Web Route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('posts',[App\Http\Controllers\PostController::class,'index'])->name('post');
+Route::get('posts/create',[App\Http\Controllers\PostController::class,'create'])->name('post.create');
+Route::post('posts/store',[App\Http\Controllers\PostController::class,'store'])->name('post.store');
